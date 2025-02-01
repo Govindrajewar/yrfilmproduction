9cee64e0f1d10190b954d68610cd0cc013ebabe7
Govind Rajewar <abhirajewar87@gmail.com>
2025-02-02 02:14:36 +0530

    ContactForm component added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import Posters from "../components/Posters";
-import AboutUs from "../components/AboutUs";
-import Projects from "../components/Projects";
-import Services from "../components/Services";
+// import Posters from "../components/Posters";
+// import AboutUs from "../components/AboutUs";
+// import Projects from "../components/Projects";
+// import Services from "../components/Services";
+import ContactForm from "../components/ContactForm";
 
 function HomePage() {
   return (
     <div className="HomePage">
-      <Posters />
+      {/* <Posters />
       <AboutUs />
       <Projects />
-      <Services />
+      <Services /> */}
+      <ContactForm/>
     </div>
   );
 }
