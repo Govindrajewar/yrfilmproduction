0b5965633106c0688b83ed847d010f34e515d147
Govind Rajewar <abhirajewar87@gmail.com>
2025-02-02 03:57:49 +0530

    env files added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/HomePage";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.REACT_APP_BASENAME}>
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
