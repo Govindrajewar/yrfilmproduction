a339b9e9a26e8162998f0ea3a90bf057040ac36e
Govind Rajewar <abhirajewar87@gmail.com>
2025-02-02 03:29:14 +0530

    NavBar Added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,52 @@
-import React from "react";
-// import Posters from "../components/Posters";
-// import AboutUs from "../components/AboutUs";
-// import Projects from "../components/Projects";
-// import Services from "../components/Services";
-// import ContactForm from "../components/ContactForm";
+import React, { useState, useEffect } from "react";
+import "../style/HomePage/HomePage.css";
+import Posters from "../components/Posters";
+import AboutUs from "../components/AboutUs";
+import Projects from "../components/Projects";
+import Services from "../components/Services";
+import ContactForm from "../components/ContactForm";
 import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
 
 function HomePage() {
+  const [showButton, setShowButton] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > window.innerHeight / 4) {
+      setShowButton(true);
+    } else {
+      setShowButton(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="HomePage">
-      {/* <Posters /> */}
-      {/* <AboutUs />
+      <NavBar/>
+      {/* <Posters />
+      <AboutUs />
       <Projects />
       <Services />
-      <ContactForm /> */}
+      <ContactForm />
       <Footer />
+      {showButton && (
+        <button type="button" className="up-btn" onClick={scrollToTop}>
+          ^
+        </button>
+      )} */}
     </div>
   );
 }
